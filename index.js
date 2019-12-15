7d90509848eea20c8abeea4ad7e8e57354a34288
fabiolademarco <fabioladm82@gmail.com>
2019-12-15 19:04:20 +0100

    add db.js e index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+// FileName: index.js
+// Import express
+let express = require('express')
+
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import MySql
+//let mysql = require('mysql');
+// Initialize the app
+let app = express();
+// Import db
+let connection = require("./db")
+//import services
+//...
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect, * means that all the website are allowed
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+
+//Import routes
+//NomeVariabileImportServices(app);
+
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});
