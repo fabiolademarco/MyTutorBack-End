0ba5d007b50af57912212bb9032dfb1bb77e626d
emdant <marcodantonio49@gmail.com>
2019-12-18 21:20:29 +0100

    Added update method in noticeControl

--- controllers/noticeControl.js
+++ controllers/noticeControl.js
@@ -27,14 +27,25 @@ exports.create = (req, res) => {
 
   Notice.create(notice, (err, data) => {
     if (err) {
-      return res.send({error: 'Errore nella creazione del bando'});
+      return res.send({error: 'Errore during notice creation'});
     }
     return res.send({notice: data});
   });
 };
 
 exports.update = (req, res) => {
+  const notice = req.body;
 
+  if (notice == null || notice == undefined) {
+    return res.status(400).send({error: 'Request body must be defined'});
+  }
+
+  Notice.update(notice, (err, data) => {
+    if (err) {
+      return res.status(400).send('Database error');
+    }
+    return res.status(200).send({notice: data});
+  });
 };
 
 exports.setStatus = (req, res) => {
@@ -54,7 +65,7 @@ exports.find = (req, res) => {
   const id = req.query.protocol;
   if (id === null || Number.parseInt(protocol) === NaN) {
     res.status(412)
-        .send(new Error('Protocollo non valido'));
+        .send({error: 'Invalid protocol number'});
     return;
   }
 
@@ -69,7 +80,7 @@ exports.find = (req, res) => {
 exports.findAll = (req, res) => {
   Notice.findAll((err, data) => {
     if (err) {
-      return res.status(414).send({error: 'Errore del database'});
+      return res.status(414).send({error: 'Database error'});
     }
     return res.status(200).send({notices: data});
   });
