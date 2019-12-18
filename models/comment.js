4b13b4180680c01090b80e154c525aefedd97d45
Pur2all <39830676+Pur2all@users.noreply.github.com>
2019-12-18 16:51:44 +0100

    Added Comment model template

---
+++ models/comment.js
@@ -0,0 +1,43 @@
+/**
+ * Comment
+ *
+ * This class represents a Comment
+ *
+ * @author Francesco Migliaro
+ * @version
+ * @since
+ *
+ * @copyright 2019 - Copyright by Gang Of Four Eyes
+ */
+
+const pool = require('../db');
+
+const table = 'comment';
+
+const Comment = function(comment) {
+
+};
+
+Comment.create = (comment, result) => {
+
+};
+
+Comment.update = (comment, result) => {
+
+};
+
+Comment.remove = (comment, result) => {
+
+};
+
+Comment.exists = (comment, result) => {
+
+};
+
+Comment.findByProtocol = (noticeProtocol, result) => {
+
+};
+
+Comment.findAll = (result) => {
+
+};
