e69c49536fad4c9f98086e9db1c7873a8ade929b
Giannandrea <gn.vicidomini@gmail.com>
2019-12-19 20:11:58 +0100

    Update applicationSheet.js

--- models/applicationSheet.js
+++ models/applicationSheet.js
@@ -1,3 +1,6 @@
+const pool = require('../db');
+const table = 'application_sheet';
+
 /**
  * Application Sheet
  *
@@ -9,114 +12,99 @@
  *
  * 2019 - Copyright by Gang Of Four Eyes
  */
-const pool = require('../db');
-const table = 'application_sheet';
-
-/**
+class ApplicationSheet {
+  /**
  * ApplicationSheet object constructor
  * @param {ApplicationSheet} applicationSheet
  */
-const ApplicationSheet = function(applicationSheet) {
-  this.noticeProtocol = applicationSheet.noticeProtocol;
-  this.documentsToAttach = applicationSheet.documentsToAttach;
-};
-
-/** Creates an application sheet
- * @param {ApplicationSheet} applicationSheet The application sheet to create
- * @param {callback} result The callback that handle the response.
- * @return {void}
- */
-ApplicationSheet.create = (applicationSheet, result) => {
-  if (applicationSheet == null) {
-    return result(new Error('Il parametro passato è nullo'), null);
+  constructor(applicationSheet) {
+    this.noticeProtocol = applicationSheet.noticeProtocol;
+    this.documentsToAttach = applicationSheet.documentsToAttach;
   }
-
-  pool.query(`INSERT INTO ${table} SET`,
-      applicationSheet,
-      (err, data)=>{
-        if (err) {
-          return result(err, null);
-        }
-        result(null, applicationSheet);
-      },
-  );
-};
-
-/** Updates an applicatin sheet
- * @param {ApplicationSheet} applicationSheet The application sheet to update
- * @param {callback} result The callback that handle the response.
- * @return {void}
- */
-ApplicationSheet.update = (applicationSheet, result) => {
-  if (applicationSheet == null) {
-    return result(new Error('Il parametro passato è nullo'), null);
-  }
-  pool.query(`UPDATE ${table} SET ? where notice_protocol = 
-  ${applicationSheet.noticeProtocol}`,
-  applicationSheet,
-  (err, data)=>{
-    if (err) {
-      return result(err, null);
+  /** Creates an application sheet
+   * @param {ApplicationSheet} applicationSheet The application sheet to create
+   * @return {Promise} Promise representing the fulfillment
+   *                   of an application sheet creation
+   */
+  static create(applicationSheet) {
+    if (applicationSheet == null) {
+      return 'Il parametro passato è nullo';
     }
-    result(null, applicationSheet);
-  },
-  );
-};
-
-/** Removes an application sheet
- * @param {ApplicationSheet} applicationSheet The application sheet to remove
- * @param {callback} result The callback that handle the response.
- * @return {void}
- */
-ApplicationSheet.remove = (applicationSheet, result) => {
-  if (applicationSheet == null) {
-    return result(new Error('Il parametro passato è nullo'), null);
+    return pool.query(`INSERT INTO ${table} SET`, applicationSheet)
+        .then((data) => {
+          return applicationSheet;
+        })
+        .catch((err) => {
+          throw err;
+        });
   }
-  pool.query(`DELETE FROM ${table} WHERE notice_protocol = 
-  ${applicationSheet.noticeProtocol}`,
-  (err, data)=>{
-    if (err) {
-      return result(err, null);
+  /** Updates an applicatin sheet
+   * @param {ApplicationSheet} applicationSheet The application sheet to update
+   * @return {Promise} Promise representing the fulfillment
+   *                   of the application sheet update
+   */
+  static update(applicationSheet) {
+    if (applicationSheet == null) {
+      return null;
     }
-    result(null, applicationSheet);
-  },
-
-  );
-};
-
-/** Finds an application sheet based on the protocol
- * @param {NoticeProtocol} noticeProtocol The notice protocol on which to filter
- * @param {callback} result The callback that handle the response.
- * @return {void}
- */
-ApplicationSheet.findByNotice = (noticeProtocol, result) => {
-  if (noticeProtocol == null) {
-    return result(new Error('Il parametro passato è nullo'), null);
+    return pool.query(`UPDATE ${table} SET ? where notice_protocol = 
+  ${applicationSheet.noticeProtocol}`, applicationSheet)
+        .then((data) => {
+          return applicationSheet;
+        })
+        .then((err) => {
+          throw err;
+        });
   }
-  pool.query(`SELECT * FROM ${table} WHERE notice_protocol = ?`,
-      noticeProtocol,
-      (err, data) => {
-        if (err) {
-          return result(err, null);
-        }
-        result(null, data);
-      });
-};
-
-
-/** Returns aplication sheet table content
- * @param {callback} result
- * @return {void}
- */
-ApplicationSheet.findAll= (result)=>{
-  pool.query(`SELECT * FROM ${table}`,
-      (err, data)=>{
-        if (err) {
-          return result(err, null);
-        }
-        result(null, data);
-      });
-};
+  /** Removes an application sheet
+   * @param {ApplicationSheet} applicationSheet The application sheet to remove
+   * @return {Promise} Promise representing the fulfillment
+   *                   of the application sheet removal
+   */
+  static remove(applicationSheet) {
+    if (applicationSheet == null) {
+      return null;
+    }
+    return pool.query(`DELETE FROM ${table} WHERE notice_protocol = 
+  ${applicationSheet.noticeProtocol}`)
+        .then(((data) => {
+          return applicationSheet;
+        }))
+        .catch((err) => {
+          throw err;
+        });
+  }
+  /** Finds an application sheet based on the protocol
+   * @param {NoticeProtocol} noticeProtocol The notice protocol on which to filter
+   * @return {Promise} Promise representing the fulfillment
+   *                   of the rapplication sheet search
+   */
+  static findByNotice(noticeProtocol) {
+    if (noticeProtocol == null) {
+      return null;
+    }
+    return pool.query(`SELECT * FROM ${table} WHERE notice_protocol = ?`, noticeProtocol)
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          throw err;
+        });
+  }
+  /** Returns application sheet table content
+   * @return {Promise} Promise representing the fulfillment
+   *                   of the application sheet search
+   */
+  static findAll() {
+    return pool.query(`SELECT * FROM ${table}`)
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          throw err;
+        });
+  }
+}
 
 
 module.exports = ApplicationSheet;
