78b54001841213004e6945648c13504647e0cc6d
Giannandrea <gn.vicidomini@gmail.com>
2019-12-24 09:50:52 +0100

    user service and index update

---
+++ services/userService.js
@@ -0,0 +1,10 @@
+const UserControl=require('../controllers/userControl');
+
+
+module.exports=function(app) {
+  app.delete('/api/users/:id', UserControl.delete);
+  app.get('/api/users/:id', UserControl.find);
+  app.patch('/api/users/', UserControl.update);
+  app.post('/api/users/search', UserControl.search);
+  app.get('/api/users/delete', UserControl.findAll);
+};
