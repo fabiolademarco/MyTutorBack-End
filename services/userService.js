5677c5aca7944516041a9a00fd5e2f2c038291f9
Giannandrea <gn.vicidomini@gmail.com>
2019-12-24 09:57:33 +0100

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -4,7 +4,7 @@ const UserControl=require('../controllers/userControl');
 module.exports=function(app) {
   app.delete('/api/users/:id', UserControl.delete);
   app.get('/api/users/:id', UserControl.find);
-  app.patch('/api/users/', UserControl.update);
+  app.patch('/api/users', UserControl.update);
   app.post('/api/users/search', UserControl.search);
   app.get('/api/users', UserControl.findAll);
 };
