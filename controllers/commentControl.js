0a432a9f16dbd1ebb088ebb32d39d283c4c4cc20
Pur2all <39830676+Pur2all@users.noreply.github.com>
2019-12-27 16:32:54 +0100

    Added Comment Controller

---
+++ controllers/commentControl.js
@@ -0,0 +1,145 @@
+const Comment = require('../models/comment');
+const ERR_CLIENT_STATUS = 412;
+const ERR_SERVER_STATUS = 500;
+
+/**
+ * CommentControl
+ *
+ * This module represents the Comment Controller
+ *
+ * @author Francesco Migliaro
+ *
+ * @copyright 2019 - Copyright by Gang Of Four Eyes
+ */
+
+/**
+  * Handles the request for create a comment.
+  * @param {Request} req
+  * @param {Response} res
+  */
+exports.create = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!req.body) {
+    res.status(ERR_CLIENT_STATUS).send({error: 'Request body must be defined'});
+  }
+
+  const comment = req.body;
+
+  Comment.create(comment)
+      .then((comment) => {
+        return res.send({comment: comment});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
+
+/**
+ * Handles the request for update a comment.
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.update = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!req.body) {
+    res.status(ERR_CLIENT_STATUS).send({error: 'Request body must be defined'});
+  }
+
+  const comment = req.body;
+
+  Comment.update(comment)
+      .then((comment) => {
+        return res.send({comment: comment});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
+
+/**
+ * Handles the request for remove a comment.
+ * @param {Request} req
+ * @param {Reponse} res
+ */
+exports.remove = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!req.body) {
+    res.status(ERR_CLIENT_STATUS).send({error: 'Request body must be defined'});
+  }
+
+  const comment = req.body;
+
+  Comment.remove(comment)
+      .then((comment) => {
+        return res.send({comment: comment});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
+
+/**
+ * Handles the request for check if a comment exists.
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.exists = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!req.body) {
+    res.status(ERR_CLIENT_STATUS).send({error: 'Request body must be defined'});
+  }
+
+  const comment = req.body;
+
+  Comment.exists(comment)
+      .then((exists) => {
+        return res.send({exists: exists});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
+
+/**
+ * Handles the request for search a comment by notice's protocol.
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.findByProtocol = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!req.body) {
+    res.status(ERR_CLIENT_STATUS).send({error: 'Request body must be defined'});
+  }
+
+  const noticeProtocol = req.body;
+
+  Comment.findByProtocol(noticeProtocol)
+      .then((comment) => {
+        return res.send({comment: comment});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
+
+/**
+ * Handles the request for get all comment.
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.findAll = (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  Comment.findAll()
+      .then((comments) => {
+        return res.send({comments: comments});
+      })
+      .catch((err) => {
+        return res.status(ERR_SERVER_STATUS).send({error: err});
+      });
+};
