a3b8475111e958795a28a3c6171ae831e97b992a
Pur2all <39830676+Pur2all@users.noreply.github.com>
2019-12-29 19:25:53 +0100

    Added checkNotice and relative checks for the fields

--- utils/check.js
+++ utils/check.js
@@ -212,3 +212,145 @@ exports.checkRatingList=(ratingList)=>{
   }
   return true;
 };
+
+/**
+ * Checks if an evaluation criterion respects the format.
+ * @param {EvaluationCriterion} evaluationCriterion Evaluation criterion to check.
+ * @return {boolean} True if it respects the format, false otherwise.
+ */
+exports.checkEvaluationCriterion = (evaluationCriterion) => {
+  if (evaluationCriterion.name.length < 1 || evaluationCriterion.name.length > 125) {
+    return false;
+  }
+  // TODO controllare se c'è già nel bando
+  if (evaluationCriterion.maxScore < 1 || evaluationCriterion.maxScore > 27) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Checks if an article respects the format.
+ * @param {Article} article Article to check.
+ * @return {boolean} True if it respects the format, false otherwise.
+ */
+exports.checkArticle = (article) => {
+  const articleInitialExp = /[A-Z a-z]+/;
+
+  if (article.initial.length < 1 || article.initial.length > 20 || !articleInitialExp.test(article.initial)) {
+    return false;
+  }
+
+  if (article.text.length < 1 || article.text.length > 20) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Checks if a notice respects the format.
+ * @param {Notice} notice Notice to check.
+ * @return {boolean} True if it respects the format, false otherwise.
+ */
+exports.checkNotice = (notice) => {
+  const noticeFundsExp = /[0-9]+(.[0-9]{2})?/;
+  const noticeDeadlineExp = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+  if (!this.checkNoticeProtocol(notice.protocol)) {
+    return false;
+  }
+
+  if (notice.description.length < 1 || notice.description.length > 300) {
+    return false;
+  }
+
+  if (notice.notice_subject.length < 1 || notice.notice_subject.length > 2000) {
+    return false;
+  }
+
+  if (notice.assignments.length < 1 || notice.assignments.length > 15) {
+    return false;
+  }
+
+  notice.assignments.forEach((assignment) => {
+    if (!this.checkAssignment(assignment)) {
+      return false;
+    }
+  });
+
+  if (notice.admission_requirements.length < 1 || notice.admission_requirements.length > 5000) {
+    return false;
+  }
+
+  if (notice.evaluation_criteria.length < 1 || notice.evaluation_criteria > 6) {
+    return false;
+  }
+
+  notice.evaluation_criteria.forEach((evaluationCriterion) => {
+    if (!this.checkEvaluationCriterion(evaluationCriterion)) {
+      return false;
+    }
+  });
+
+  if (notice.assessable_titles.length < 1 || notice.assessable_titles.length > 5000) {
+    return false;
+  }
+
+  if (notice.how_to_submit_applications.length < 1 || notice.how_to_submit_applications.length > 5000) {
+    return false;
+  }
+
+  if (notice.selection_board.length < 1 || notice.selection_board.length > 5000) {
+    return false;
+  }
+
+  if (notice.acceptance.length < 1 || notice.acceptance.length > 5000) {
+    return false;
+  }
+
+  if (notice.incompatibility.length < 1 || notice.incompatibility > 5000) {
+    return false;
+  }
+
+  if (notice.termination_of_the_assignment.length < 1 || notice.termination_of_the_assignment > 5000) {
+    return false;
+  }
+
+  if (notice.nature_of_the_assignment.length < 1 || notice.nature_of_the_assignment.length > 5000) {
+    return false;
+  }
+
+  if (notice.unused_funds.length < 1 || notice.unused_funds.length > 5000) {
+    return false;
+  }
+
+  if (notice.responsible_for_the_procedure.length < 1 || notice.responsible_for_the_procedure.length > 5000) {
+    return false;
+  }
+
+  if (notice.notice_funds < 1 || !noticeFundsExp.test(notice.notice_funds)) {
+    return false;
+  }
+
+  if (notice.type.length < 1 || notice.type.length > 50) {
+    return false;
+  }
+
+  if (!noticeDeadlineExp.test(notice.deadline)) {
+    return false;
+  }
+
+  if (notice.articles.length < 1 || notice.articles.length > 20) {
+    return false;
+  }
+
+  notice.articles.forEach((articlee) => {
+    if (!this.checkArticle(article)) {
+      return false;
+    }
+  });
+
+  return true;
+};
