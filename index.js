0a3a6dd73bb081aeb0c2ef3297ca24ae86d2bab7
Rob11001 <robertobruno1998@libero.it>
2019-12-31 12:35:56 +0100

    Another try

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
       'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   );
 
-  res.setHeader(
+  res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   );
