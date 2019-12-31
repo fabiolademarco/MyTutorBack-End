5f4c1a045cfcae48c9b5535b15be30b31c62bb39
Rob11001 <robertobruno1998@libero.it>
2019-12-31 14:26:25 +0100

    Added documentation to User Service

--- services/userService.js
+++ services/userService.js
@@ -8,17 +8,46 @@ module.exports=function(app, auth) {
 
   /**
    * @api {DELETE} /api/users/:id Delete User
-   * @apiName Delete user
+   * @apiName DeleteUser
    * @apiGroup User
    * @apiPermission Teaching Office
-   * @apiParam {User} user User, with email and password,who wants to do the login in the system.
-   * @apiSuccess {User} user The user who logged in the system.
+   * @apiParam {User} user The user to delete
    * @apiSuccess {boolean} status The status of the operation.
-   * @apiSuccess {string} token The jwt token for authorization.
    */
-  app.delete('/api/users/:id', auth.isTeachingOffice, UserControl.delete);
+  app.delete('/api/users', auth.isTeachingOffice, UserControl.delete);
+  /**
+   * @api {GET} /api/users/:id Find an user
+   * @apiName FindUser
+   * @apiGroup User
+   * @apiPermission User
+   * @apiParam {string} id The email of the user
+   * @apiSuccess {User} user The user with the given id
+   */
   app.get('/api/users/:id', UserControl.find);
+  /**
+   * @api {PATCH} /api/users Update an User
+   * @apiName UpdateUser
+   * @apiGroup User
+   * @apiPermission User
+   * @apiParam {User} user The user to update
+   * @apiSuccess {User} user The updated User
+   */
   app.patch('/api/users', UserControl.update);
+  /**
+   * @api {POST} /api/users/search Searches users
+   * @apiName SearchUser
+   * @apiGroup User
+   * @apiPermission Teaching Office
+   * @apiParam {Object} param Object with name, surname, role or verified to do the search
+   * @apiSuccess {User[]} list List of the searched users
+   */
   app.post('/api/users/search', auth.isTeachingOffice, UserControl.search);
+  /**
+   * @api {GET} /api/users Find all users
+   * @apiName FindAll
+   * @apiGroup User
+   * @apiPermission Teaching Office
+   * @apiSuccess {User[]} list List of all users
+   */
   app.get('/api/users', auth.isTeachingOffice, UserControl.findAll);
 };
