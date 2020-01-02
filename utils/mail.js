3f7e7f19701724fbcb35d1eaddf1fa0c31189e91
Rob11001 <robertobruno1998@libero.it>
2020-01-02 16:08:17 +0100

    Implemented the mail comunication

--- utils/mail.js
+++ utils/mail.js
@@ -10,7 +10,7 @@ const user = {
   password: 'MyTutorPassword',
 };
 const domain = 'localhost:3000'; // Mettere nel .env
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // To solve a problem of security of the front end requests
 
 exports.sendEmailToProfessor = (receiver, token) => {
   const transport = createTransport();
@@ -25,6 +25,66 @@ exports.sendEmailToProfessor = (receiver, token) => {
     html: body,
   };
   return transport.sendMail(mailOptions); // Return a promise to check if there is an error
+};
+
+exports.sendEmailToStudentRequest = (receiver) => {
+  const transport = createTransport();
+  const email = template.toRequestStudent;
+  let body = email.body;
+  body = `${body}${email.link.substring(0, 17)}"http://${domain}/"${email.link.substring(19)}`;
+  body = `${body}${email.greetings}`;
+  const mailOptions = {
+    from: user.email,
+    to: receiver,
+    subject: email.subject,
+    html: body,
+  };
+  return transport.sendMail(mailOptions);
+};
+
+exports.sendEmailToTeachingOfficeBook = (receiver, assignment) => {
+  const transport = createTransport();
+  const email = template.toTeachingOfficeBook;
+  let body = `${email.intro}${assignment.student} ${email.body}${assignment.code} ${email.end}`;
+  body = `${body}${email.link.substring(0, 17)}"http://${domain}/"${email.link.substring(19)}`;
+  body = `${body}${email.greetings}`;
+  const mailOptions = {
+    from: user.email,
+    to: receiver,
+    subject: email.subject,
+    html: body,
+  };
+  return transport.sendMail(mailOptions);
+};
+
+exports.sendEmailToStudentAssign = (receiver, assignmentCode) => {
+  const transport = createTransport();
+  const email = template.toStudentAssign;
+  let body = `${email.intro}${assignmentCode} ${email.body}`;
+  body = `${body}${email.link.substring(0, 17)}"http://${domain}/"${email.link.substring(19)}`;
+  body = `${body}${email.greetings}`;
+  const mailOptions = {
+    from: user.email,
+    to: receiver,
+    subject: email.subject,
+    html: body,
+  };
+  return transport.sendMail(mailOptions);
+};
+
+exports.sendEmailToTeachingOfficeDecline = (receiver, student, assignmentCode) => {
+  const transport = createTransport();
+  const email = template.toTeachingOfficeDecline;
+  let body = `${email.intro}${student} ${email.body}${assignmentCode} </p>`;
+  body = `${body}${email.link.substring(0, 17)}"http://${domain}/"${email.link.substring(19)}`;
+  body = `${body}${email.greetings}`;
+  const mailOptions = {
+    from: user.email,
+    to: receiver,
+    subject: email.subject,
+    html: body,
+  };
+  return transport.sendMail(mailOptions);
 };
 
 createTransport = () => {
