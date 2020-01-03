d71d82eea7872606cfa8693437ded45c9a469b4d
Pur2all <39830676+Pur2all@users.noreply.github.com>
2020-01-03 16:30:57 +0100

    Added Test_ModificaBozzaDomanda's tests

--- testing/controllers/applicationSheetControlTest.js
+++ testing/controllers/applicationSheetControlTest.js
@@ -23,19 +23,19 @@ let res;
 const OK_STATUS = 200;
 const ERR_CLIENT_STATUS = 412;
 
-describe('Gestione Avvio', function() {
+describe('Controller ApplicationSheet', function() {
   let applicationSheet;
 
-  describe('Test_CreaBozzaDomanda', function() {
-    beforeEach(function() {
-      applicationSheet = {
-        notice_protocol: 'Prot. n. 0274751',
-        documents_to_attach: 'dichiarazione sostitutiva di certificazione relativa all\'avvenuto conseguimento del diploma di laurea magistrale, alla votazione riportata ed alla data del suo conseguimento;   - dichiarazione sostitutiva di certificazione all\'iscrizione al corso di dottorato di ricerca con sede presso l\'Università degli Studi di Salerno;   - dichiarazione sostitutiva di certificazione, resa ai sensi del D.P.R. 445/2000, relativa a precedenti attività di tutorato, didattico-integrative, propedeutiche e di recupero in ambito universitario',
-      };
-      req = mockRequest({method: 'PUT', body: {applicationSheet: applicationSheet}});
-      res = mockResponse();
-    });
+  beforeEach(function() {
+    applicationSheet = {
+      notice_protocol: 'Prot. n. 0274751',
+      documents_to_attach: 'dichiarazione sostitutiva di certificazione relativa all\'avvenuto conseguimento del diploma di laurea magistrale, alla votazione riportata ed alla data del suo conseguimento;   - dichiarazione sostitutiva di certificazione all\'iscrizione al corso di dottorato di ricerca con sede presso l\'Università degli Studi di Salerno;   - dichiarazione sostitutiva di certificazione, resa ai sensi del D.P.R. 445/2000, relativa a precedenti attività di tutorato, didattico-integrative, propedeutiche e di recupero in ambito universitario',
+    };
+    req = mockRequest({method: 'PUT', body: {applicationSheet: applicationSheet}});
+    res = mockResponse();
+  });
 
+  describe('Test_CreaBozzaDomanda', function() {
     it('TCS_AV.3.0', function() {
       applicationSheet.documents_to_attach = '';
       applicationSheetControl.create(req, res);
@@ -50,6 +50,25 @@ describe('Gestione Avvio', function() {
 
     it('TCS_AV.3.2', async function() {
       await applicationSheetControl.create(req, res);
+      expect(res.status).to.have.been.calledWith(OK_STATUS);
+    });
+  });
+
+  describe('Test_ModificaBozzaDomanda', function() {
+    it('TCS_AV.4.0', function() {
+      applicationSheet.documents_to_attach = '';
+      applicationSheetControl.update(req, res);
+      expect(res.status).to.have.been.calledWith(ERR_CLIENT_STATUS);
+    });
+
+    it('TCS_AV.4.1', function() {
+      applicationSheet.documents_to_attach = 'a'.repeat(5001);
+      applicationSheetControl.update(req, res);
+      expect(res.status).to.have.been.calledWith(ERR_CLIENT_STATUS);
+    });
+
+    it('TCS_AV.4.2', async function() {
+      await applicationSheetControl.update(req, res);
       expect(res.status).to.have.been.calledWith(OK_STATUS);
     });
   });
