28db2b1b667ca31b07e34cec5ca986e048a1eb82
Pur2all <39830676+Pur2all@users.noreply.github.com>
2020-01-08 16:47:32 +0100

    Marco's fix on my pc

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
 
-const url = process.env.DATABASE_URI;
+let url = process.env.DATABASE_URI;
 
 const dateStrings = '?dateStrings=true';
 
 if (!url.includes(dateStrings)) {
-  url.concat(url, dateStrings);
+  url = url.concat(dateStrings);
 }
+
 const pool = mysql.createPool(url);
 
 module.exports = pool;
