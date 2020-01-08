921943759690522e73adb4f0551bd0a1e5b93074
Pur2all <39830676+Pur2all@users.noreply.github.com>
2020-01-08 10:32:01 +0100

    Fixed tests

--- testing/models/ratingTest.js
+++ testing/models/ratingTest.js
@@ -13,9 +13,8 @@ const {expect} = chai;
 
 const Rating = require('../../models/rating');
 const Student = require('../../models/student');
-const db = require('../../db');
-const table = 'rating';
-
+const Notice = require('../../models/notice');
+const exampleNotice = require('./exampleNotices.json');
 
 const studentConst = {
   email: 'f.migliaro69@studenti.unisa.it',
@@ -23,14 +22,14 @@ const studentConst = {
   surname: 'Migliaro',
   role: 'Student',
   verified: '1',
-  registration_number: 'aaaaBBBB11112222',
+  registration_number: 'aaaaB1122',
   password: 'Abcde123',
   birth_date: '1998-03-03 ',
 };
 
 const ratingConst = {
-  student: studentConst,
-  assignment_id: 3,
+  student: JSON.parse(JSON.stringify(studentConst)),
+  assignment_id: null,
   titles_score: 10,
   interview_score: 10,
 };
@@ -38,89 +37,101 @@ const ratingConst = {
 describe('Rating model', function() {
   let rating;
 
+  before(async function() {
+    const notice = await Notice.create(exampleNotice.notice);
+
+    await Student.create(studentConst);
+
+    ratingConst.assignment_id = notice.assignments[0].id;
+    rating = JSON.parse(JSON.stringify(ratingConst));
+  });
+
+  after(async function() {
+    await Notice.remove(exampleNotice.notice);
+    await Student.delete(studentConst);
+  });
+
   describe('Create method', function() {
     beforeEach(function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
+
+      await Rating.remove(rating);
     });
 
-    it('Create_1', async () => {
-      await expect(Rating.create(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('Create_1', function() {
+      expect(Rating.create(null)).to.be.rejectedWith(Error, /The rating must not be null/);
     });
 
-    it('Create_2', async function() {
+    it('Create_2', function() {
       rating.student.email = 'tralalalalaliday';
-      await expect(Rating.create(rating)).to.be.rejectedWith(Error);
+
+      expect(Rating.create(rating)).to.be.rejectedWith(Error);
     });
 
-    it('Create_3', async function() {
+    it('Create_3', function() {
+      rating.student.email = studentConst.email;
+
       expect(Rating.create(rating)).to.be.fulfilled;
     });
   });
 
   describe('Update method', function() {
-    beforeEach(async function() {
+    before(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
 
       await Rating.create(rating);
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
+
+      await Rating.remove(rating);
     });
 
-    it('Update_1', async function() {
-      await expect(Rating.update(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('Update_1', function() {
+      expect(Rating.update(null)).to.be.rejectedWith(Error, /The rating must not be null/);
     });
 
-    it('Update_2', async function() {
+    it('Update_2', function() {
       rating.assignment_id = 0;
-      await expect(Rating.update(rating)).to.be.rejectedWith(Error, /The rating doesn't exists/);
+
+      expect(Rating.update(rating)).to.be.rejectedWith(Error, /The rating doesn't exists/);
     });
 
     it('Update_3', async function() {
+      rating.assignment_id = ratingConst.assignment_id;
+
       expect(Rating.update(rating)).to.be.fulfilled;
     });
   });
 
   describe('Remove method', function() {
-    beforeEach(async function() {
+    before(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
 
       await Rating.create(rating);
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
+
+      await Rating.remove(rating);
     });
 
-    it('Remove_1', async function() {
-      await expect(Rating.remove(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('Remove_1', function() {
+      expect(Rating.remove(null)).to.be.rejectedWith(Error, /The rating must not be null/);
     });
 
-    it('Remove_2', async function() {
+    it('Remove_2', function() {
       expect(Rating.remove(rating)).to.be.fulfilled;
     });
   });
 
   describe('Exists method', function() {
-    beforeEach(async function() {
-      rating = JSON.parse(JSON.stringify(ratingConst));
-
-      await Rating.create(rating);
-    });
-
-    afterEach(async function() {
-      rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
-    });
-
     it('Exists_1', async function() {
       await expect(Rating.exists(null)).to.be.rejectedWith(Error, /The rating must not be null/);
     });
@@ -131,95 +142,82 @@ describe('Rating model', function() {
   });
 
   describe('FindById method', function() {
-    beforeEach(async function() {
+    before(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
 
       await Rating.create(rating);
-      await Student.create(studentConst);
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
-      await db.query(`DELETE FROM student WHERE user_email=?`, [ratingConst.student.email]);
+
+      await Rating.remove(rating);
     });
 
-    it('FindById_1', async function() {
-      await expect(Rating.findById(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('FindById_1', function() {
+      expect(Rating.findById(null)).to.be.rejectedWith(Error, /valid/);
     });
 
-    it('FindById_2', async function() {
+    it('FindById_2', function() {
       rating.assignment_id = 0;
-      await expect(Rating.findById(rating.student.email, rating.assignment_id)).to.be.rejectedWith(Error, 'No result was found');
+
+      expect(Rating.findById(rating.student.email, rating.assignment_id)).to.be.rejectedWith(Error, 'No result was found');
     });
 
     it('FindById_3', function() {
-      expect(Rating.findById(rating.student.name, rating.assignment_id)).to.be.fulfilled;
+      rating.assignment_id = ratingConst.assignment_id;
+
+      expect(Rating.findById(rating.student.email, rating.assignment_id)).to.be.fulfilled;
     });
   });
 
   describe('FindByStudent method', function() {
-    beforeEach(async function() {
+    before(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
 
       await Rating.create(rating);
-      await Student.create(studentConst);
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
-      await db.query(`DELETE FROM student WHERE user_email=?`, [ratingConst.student.email]);
+
+      await Rating.remove(rating);
     });
 
-    it('FindByStudent_1', async function() {
-      await expect(Rating.findByStudent(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('FindByStudent_1', function() {
+      expect(Rating.findByStudent(null)).to.be.rejectedWith(Error, /null/);
     });
 
-    it('FindByStudent_2', async function() {
-      await expect(Rating.findByStudent(rating.student.email)).to.be.fulfilled;
+    it('FindByStudent_2', function() {
+      expect(Rating.findByStudent(rating.student.email)).to.be.fulfilled;
     });
   });
 
   describe('FindByAssignment method', function() {
-    beforeEach(async function() {
+    before(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
 
       await Rating.create(rating);
-      await Student.create(studentConst);
     });
 
-    afterEach(async function() {
+    after(async function() {
       rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
-      await db.query(`DELETE FROM student WHERE user_email=?`, [ratingConst.student.email]);
+
+      await Rating.remove(rating);
     });
 
-    it('FindByAssignment_1', async function() {
-      await expect(Rating.findByAssignment(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+    it('FindByAssignment_1', function() {
+      expect(Rating.findByAssignment(null)).to.be.rejectedWith(Error, /null/);
     });
 
-    it('FindByAssignment_2', async function() {
-      await expect(Rating.findByAssignment(rating.student.assignment_id)).to.be.fulfilled;
+    it('FindByAssignment_2', function() {
+      expect(Rating.findByAssignment(rating.assignment_id)).to.be.fulfilled;
     });
   });
 
   describe('FindByProtocol method', function() {
-    beforeEach(async function() {
-      rating = JSON.parse(JSON.stringify(ratingConst));
-
-      await Rating.create(rating);
-      await Student.create(studentConst);
-    });
-
-    afterEach(async function() {
-      rating = JSON.parse(JSON.stringify(ratingConst));
-      await db.query(`DELETE FROM ${table} WHERE student=? AND assignment_id=?`, [rating.student.email, rating.assignment_id]);
-      await db.query(`DELETE FROM student WHERE user_email=?`, [ratingConst.student.email]);
-    });
-
     it('FindByProtocol_1', async function() {
-      await expect(Rating.findByProtocol(null)).to.be.rejectedWith(Error, /The rating must not be null/);
+      await expect(Rating.findByProtocol(null)).to.be.rejectedWith(Error, /null/);
     });
 
     it('FindByProtocol_2', async function() {
